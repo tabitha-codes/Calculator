70b0fc446c6e970fad6061b3f3ae2828884b4280
tabitha-codes <tabithacodes@gmail.com>
2026-08-02 19:13:51 -0500

    calculator numbers

---
+++ calculator.js
@@ -0,0 +1,40 @@
+
+
+
+// // ===========ADDITION===========
+
+// function sum(a, b) {
+// console.log( a + b );
+// }
+// sum(5, 5);
+
+
+// // ===========SUBTRACTION===========
+// function subtraction(a, b) {
+// console.log( a - b );
+// }
+// subtraction(5, 5);
+
+
+// // ===========MULTIPICATION===========
+// function multiply(a, b) {
+// console.log( a * b );
+// }
+// multiply(5, 5);
+
+
+// // ===========DIVISION===========
+// function division(a, b) {
+// console.log( a / b );
+// }
+// division(5, 5);
+
+// Receives a value — notice the (number) in the parentheses
+function appendNumber(number) {
+  console.log("Number clicked:", number);
+}
+
+// Receives nothing — empty parentheses
+function clearDisplay() {
+  console.log("Clear clicked");
+}
